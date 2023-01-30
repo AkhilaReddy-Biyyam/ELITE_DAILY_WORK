3b327728beceb3e887d1bf5b979fc9d5fa7986d3
Akhila Biyyam <20pa1a0520@vishnu.edu.in>
2023-01-31 00:23:41 +0530

    Create ceil.java

---
+++ 30-01-2023/ceil.java
@@ -0,0 +1,41 @@
+import java.util.* ;
+import java.io.*; 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T>
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+************************************************************/
+
+public class Solution {
+
+    public  static int findCeil(TreeNode<Integer> root, int x) {
+        // Write your code here
+        int ceil=-1;
+        while(root!=null){
+            if(root.data>=x)
+                ceil=root.data;
+            if(x==root.data)
+                return (int)root.data;
+            else if(x<root.data)
+                root=root.left;
+            else
+                root=root.right;
+        }
+        return ceil;
+    }
+}
