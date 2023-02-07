e07788542d4fbe1c5d78370a0494efece8dbe19d
Akhila Biyyam <20pa1a0520@vishnu.edu.in>
2023-02-08 00:44:39 +0530

    Create time_inform_all_emp.java

---
+++ 07-02-2023/time_inform_all_emp.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int max=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,dfs(i,manager,informTime));
+        }
+        return max;
+    }
+    public int dfs(int i,int[] manager, int[] informTime){
+        if(manager[i]!=-1){
+            informTime[i]+=dfs(manager[i],manager,informTime);
+            manager[i]=-1;
+        }
+        return informTime[i];
+    }
+}
+
+ 
