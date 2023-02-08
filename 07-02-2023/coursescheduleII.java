7e16ff2c5facaa8ce2f5aa9d62c089383dc393d2
Akhila Biyyam <20pa1a0520@vishnu.edu.in>
2023-02-08 23:10:46 +0530

    Create coursescheduleII.java

---
+++ 07-02-2023/coursescheduleII.java
@@ -0,0 +1,47 @@
+class Solution {
+    static ArrayList<ArrayList<Integer>>adjList;
+    static boolean [] visited;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int indegree[] = new int[numCourses];
+        adjList=new ArrayList<>(numCourses);
+        for(int i=0;i<numCourses;i++){
+            adjList.add(new ArrayList<Integer>());
+        }
+        for(int j=0;j<prerequisites.length;j++){
+            int vertex1=prerequisites[j][0];
+            int vertex2=prerequisites[j][1];
+            adjList.get(vertex2).add(vertex1);
+            indegree[vertex1]++;
+        }
+        visited=new boolean[numCourses+1];
+        Queue<Integer>queue=new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                queue.add(i);
+            }
+        }
+        ArrayList<Integer>res=new ArrayList<>();
+        while(!queue.isEmpty()){
+            int curr=queue.poll();
+            res.add(curr);
+            for(int neighbour:adjList.get(curr)){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    queue.add(neighbour);
+                }
+            }
+        }
+        if(res.size()!=numCourses)
+            return new int[0];
+        int [] res_arr=new int[res.size()];
+        for(int i=0;i<res.size();i++){
+            res_arr[i]=res.get(i);
+        }
+        return res_arr;
+        
+    }
+}
+
+
+
+
